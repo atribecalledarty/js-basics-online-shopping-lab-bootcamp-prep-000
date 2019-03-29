17d31805837ec4c96e0ca53a801189acfe734302
Arthur Song <arth3rs0ng@gmail.com>
2019-03-29 03:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,11 +66,11 @@ function removeFromCart(item) {
       removed = cart.splice(i, 1)
     }
   }
-    if (removed === ''){
-      return 'That item is not in your cart.'
-    } else {
-      return cart
-    }
+  if (removed === ''){
+    return 'That item is not in your cart.'
+  } else {
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
